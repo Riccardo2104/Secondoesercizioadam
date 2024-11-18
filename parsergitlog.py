e2ef84b9bff923844884979f4c55ad84de76d008
sceva3 <riccardovigano21@gmail.com>
2024-11-18 17:27:25 +0100

    creazione del file commit.txt

--- parsergitlog.py
+++ parsergitlog.py
@@ -8,3 +8,10 @@ result = subprocess.run(["powershell.exe", command], capture_output=True, text=T
 
 stringa = result.stdout
 
+# creazione del file dalla stringa
+text_file = open(r"C:\Users\Riccardo\Desktop\Progetti\Secondoesercizioadam\commit.txt", "w")
+
+# write string to file
+text_file.write(stringa)
+
+text_file.close()
