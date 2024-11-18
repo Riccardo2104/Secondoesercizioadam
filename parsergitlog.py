266c44cbfbceef321fc719ddb38112d7cb3c5681
sceva3 <riccardovigano21@gmail.com>
2024-11-18 17:32:01 +0100

    estrazione dei commit dal file commit.txt

--- parsergitlog.py
+++ parsergitlog.py
@@ -1,6 +1,7 @@
 import subprocess
 import re
 
+#creazione della stringa 
 command = r"cd C:\Users\Riccardo\Desktop\Esercizioadam; git log"
 
 
@@ -11,7 +12,19 @@ stringa = result.stdout
 # creazione del file dalla stringa
 text_file = open(r"C:\Users\Riccardo\Desktop\Progetti\Secondoesercizioadam\commit.txt", "w")
 
-# write string to file
+
 text_file.write(stringa)
 
 text_file.close()
+
+#estrazione dei commit dal file commit.txt  
+commit = []
+secondo_percorso =r"C:\Users\Riccardo\Desktop\Progetti\Secondoesercizioadam\commit.txt"
+with open(secondo_percorso) as myfile:
+    for line in myfile.readlines():
+        if re.search(r'commit [A-Za-z0-9]+', line):                
+            commit.append(line.replace("\n", ""))
+
+
+
+print(commit)
